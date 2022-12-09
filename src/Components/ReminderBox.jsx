8948db4f450010b0152dd7e6df02cb46ed2f5293
shivraj32644 <shivraj32644@gmail.com>
2022-12-09 23:38:18 +0530

    barchart work completed

--- src/Components/ReminderBox.jsx
+++ src/Components/ReminderBox.jsx
@@ -1,18 +1,31 @@
-import React from 'react'
-import styles from '../Styles/reminder.module.css'
-import corpcare from '../Assets/corpcare.png'
+import React from "react";
+import styles from "../Styles/reminder.module.css";
+import corpcare from "../Assets/corpcare.png";
+import { useToast } from "@chakra-ui/react";
 export const ReminderBox = () => {
+  const toast = useToast();
+  const handleClick = () => {
+    toast({
+      title: `Reminder Added`,
+      status: "success",
+      isClosable: true,
+    });
+  };
   return (
-    <div className={styles.container} >
+    <div className={styles.container}>
       <p>Reminder</p>
-      
-    
-      <div className={styles.reminder} >
-        <p>Agency Design Kit</p>
+
+      <div className={styles.reminder}>
+        <p style={{ left: "38px" }}>Agency Design Kit</p>
         <img src={corpcare} alt="logo" />
-        <p className={styles.detail}>Will be published Nov 25, 2022</p>
-        <button>Set as Reminder</button>
+        <p
+          style={{ top: "147px", lineHeight: "141.5%" }}
+          className={styles.detail}
+        >
+          Will be published Nov 25, 2022
+        </p>
+        <button onClick={handleClick}>Set as Reminder</button>
       </div>
     </div>
-  )
-}
+  );
+};
---
+++ src/Data.js
@@ -0,0 +1,44 @@
+export const savedMoney = [
+    {
+        id: 1,
+        day: "S",
+        saved: 10,
+        expense:30
+    },
+    {
+        id: 2,
+        day: "M",
+        saved: 20,
+        expense:25
+    },
+    {
+        id: 3,
+        day: "T",
+        saved: 20,
+        expense:20
+    },
+    {
+        id: 4,
+        day: "W",
+        saved: 10,
+        expense:30
+    },
+    {
+        id: 5,
+        day: "T",
+        saved: 16,
+        expense:22
+    },
+    {
+        id: 6,
+        day: "F",
+        saved: 15,
+        expense:28
+    },
+    {
+        id: 7,
+        day: "S",
+        saved: 24,
+        expense:29
+    },
+]
