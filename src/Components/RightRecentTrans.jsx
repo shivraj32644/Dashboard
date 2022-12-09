a589563097d9f5cfb44b970597b24acb12b17c64
shivraj32644 <shivraj32644@gmail.com>
2022-12-10 00:44:08 +0530

    barchart work completed

--- src/Components/RightRecentTrans.jsx
+++ src/Components/RightRecentTrans.jsx
@@ -1,11 +1,28 @@
 import { Box } from '@chakra-ui/react'
 import React from 'react'
 import styles from '../Styles/RightRecentTrans.module.css'
+import DurationTab from './DurationTab'
 
 export const RightRecentTrans = () => {
   return (
-      <Box className={styles.container} >
-          Recent Transaction
+    <Box className={styles.container} >
+      <p className={styles.title}> Recent Transaction </p>
+      <Box className={styles.chartBox} >
+        <Box className={styles.details} >
+          <p>Accounts reached</p>
+          <p>11,756</p>
+          <Box className={styles.tabs}>
+          {/* <h3>Day</h3>
+          <h3>Week</h3>
+          <h3>Month</h3>
+          <h3>Year</h3> */}
+          <DurationTab/>
+        </Box>
+        </Box>
+        
+        <Box>
+          </Box>
+      </Box>
       </Box>
   )
 }
