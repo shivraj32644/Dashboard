10db38b811678a55f7c09703aeb41b2e7390a16c
Shivraj Dangi <shivraj32644@gmail.com>
2023-01-10 10:17:00 +0530

    assets

--- Frontend/src/Components/Chart/BarChart.jsx
+++
@@ -1,9 +0,0 @@
-import { CheckboxGroup } from "@chakra-ui/react";
-import React from "react";
-import { useState } from "react";
-import { Bar } from "react-chartjs-2";
-import { chart as ChartJS } from "chart.js/auto";
-
-export const BarChart = ({ data, opt }) => {
-  return <Bar data={data} options={opt} />;
-};
--- Frontend/src/Components/Chart/PieChart.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Doughnut} from 'react-chartjs-2'
-import { chart as ChartJS } from 'chart.js'
-export const PieChart = ({data, opt }) => {
-  return (
-    <Doughnut data={data} options={opt} />
-  )
-}
